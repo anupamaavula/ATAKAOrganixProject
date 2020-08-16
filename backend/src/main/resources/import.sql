4d6c08b300a24af4b7cb9d633ef81a4d09254f95
anupama <anupama_avula@yahoo.com>
2020-08-16 11:14:25 -0400

    Worked on DB

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -48,20 +48,51 @@ INSERT INTO "public"."product_in_order" VALUES (2147483633, 1,1, 'Kids Party Foo
 -- Records of product_info
 -- ----------------------------
 
-INSERT INTO "public"."product_info" VALUES ('B0003', 0, '2018-03-10 10:37:39', 'Oranges', 'https://images.all-free-download.com/images/graphiclarge/fruits_orange_citrus_fruits_215459.jpg', 'Healthy', 10.00, 1, 200, '2018-03-10 19:42:02');
+INSERT INTO "public"."product_info" VALUES ('B0003', 0, '2018-03-10 10:37:39', 'Oranges', 'https://images.all-free-download.com/images/graphiclarge/orange_oranges_fruit_264991.jpg', 'Healthy', 10.00, 1, 200, '2018-03-10 19:42:02');
 
 --INSERT INTO "public"."product_info" VALUES ('B0003', 0, '2018-03-10 10:37:39', 'Java EE', 'https://covers.oreillystatic.com/images/9780596516680/lrg.jpg', 'Head First', 10.00, 1, 200, '2018-03-10 19:42:02');
-INSERT INTO "public"."product_info" VALUES ('C0003', 2, '2018-03-10 12:12:46', 'Mans', 'https://img1.newchic.com/thumb/view/oaupload/newchic/images/00/30/df8a1f83-035c-4942-93d6-49933ac52a34.jpg', 'Coats', 22.00, 0, 222, '2018-03-10 12:12:46');
-INSERT INTO "public"."product_info" VALUES ('D0001', 3, '2018-03-10 06:51:03', 'Everyone likes it', 'https://www.thesun.co.uk/wp-content/uploads/2017/03/nintchdbpict000277254629.jpg?strip=all&w=676', 'Coca Cola', 1.00, 0, 100, '2018-03-10 12:04:13');
-INSERT INTO "public"."product_info" VALUES ('B0002', 0, '2018-03-10 10:35:43', 'Learn Spring', 'https://images-na.ssl-images-amazon.com/images/I/51gHy16h5TL._SX397_BO1,204,203,200_.jpg', 'Spring In Action', 20.00, 0, 195, '2018-03-10 10:35:43');
-INSERT INTO "public"."product_info" VALUES ('C0001', 2, '2018-03-10 12:09:41', 'Under Armour', 'https://assets.academy.com/mgen/33/20088533.jpg?is=500,500', 'T-shirt', 10.00, 0, 109, '2018-03-10 12:09:41');
-INSERT INTO "public"."product_info" VALUES ('C0002', 2, '2018-03-10 12:11:51', 'Boys Clothes', 'https://d2ul0w83gls0j4.cloudfront.net/taxonomy/300/0102/20171024151632.jpg', 'Shirts', 13.00, 0, 108, '2018-03-10 12:11:51');
-INSERT INTO "public"."product_info" VALUES ('B0001', 0, '2018-03-10 06:44:25', 'Books for learning Java', 'https://images-na.ssl-images-amazon.com/images/I/41f6Rd6ZEPL._SX363_BO1,204,203,200_.jpg', 'Core Java', 30.00, 0, 96, '2018-03-10 06:44:25');
-INSERT INTO "public"."product_info" VALUES ('B0004', 0, '2018-03-10 10:39:29', 'Java SE', 'https://www.pearsonhighered.com/assets/bigcovers/0/1/3/2/0132778041.jpg', 'Effective Java', 30.00, 0, 199, '2018-03-10 10:39:32');
-INSERT INTO "public"."product_info" VALUES ('B0005', 0, '2018-03-10 10:40:35', 'Java SE', 'https://images-na.ssl-images-amazon.com/images/I/51S8VRHA2FL._SX357_BO1,204,203,200_.jpg', 'Thinking in Java', 30.00, 0, 199, '2018-03-10 10:40:35');
+
+
+INSERT INTO "public"."product_info" VALUES ('C0003', 2, '2018-03-10 12:12:46', 'Mango Flavor', 'https://image.freepik.com/free-photo/fresh-mango-smoothie_1339-1485.jpg', 'Mangoes', 22.00, 0, 222, '2018-03-10 12:12:46');
+--INSERT INTO "public"."product_info" VALUES ('C0003', 2, '2018-03-10 12:12:46', 'Mans', 'https://img1.newchic.com/thumb/view/oaupload/newchic/images/00/30/df8a1f83-035c-4942-93d6-49933ac52a34.jpg', 'Coats', 22.00, 0, 222, '2018-03-10 12:12:46');
+
+
+INSERT INTO "public"."product_info" VALUES ('D0001', 3, '2018-03-10 06:51:03', 'Strawberry Muffin', 'https://img.freepik.com/free-photo/strawberry-muffin-white-plate-with-fresh-strawberry_2829-14443.jpg?size=626&ext=jpg', 'Muffin', 1.00, 0, 100, '2018-03-10 12:04:13');
+--INSERT INTO "public"."product_info" VALUES ('D0001', 3, '2018-03-10 06:51:03', 'Everyone likes it', 'https://www.thesun.co.uk/wp-content/uploads/2017/03/nintchdbpict000277254629.jpg?strip=all&w=676', 'Coca Cola', 1.00, 0, 100, '2018-03-10 12:04:13');
+
+INSERT INTO "public"."product_info" VALUES ('B0002', 0, '2018-03-10 10:35:43', 'Bananas', 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Bananas_%28white_background%29.jpg', 'Tasty', 20.00, 0, 195, '2018-03-10 10:35:43');
+
+--INSERT INTO "public"."product_info" VALUES ('B0002', 0, '2018-03-10 10:35:43', 'Learn Spring', 'https://images-na.ssl-images-amazon.com/images/I/51gHy16h5TL._SX397_BO1,204,203,200_.jpg', 'Spring In Action', 20.00, 0, 195, '2018-03-10 10:35:43');
+
+
+INSERT INTO "public"."product_info" VALUES ('C0001', 2, '2018-03-10 12:09:41', 'Strawberry smoothie', 'https://upload.wikimedia.org/wikipedia/commons/e/eb/Strawberry_and_lemon_smoothie_%2814430283996%29.jpg', 'smoothie', 10.00, 0, 109, '2018-03-10 12:09:41');
+--INSERT INTO "public"."product_info" VALUES ('C0001', 2, '2018-03-10 12:09:41', 'Under Armour', 'https://assets.academy.com/mgen/33/20088533.jpg?is=500,500', 'T-shirt', 10.00, 0, 109, '2018-03-10 12:09:41');
+
+
+INSERT INTO "public"."product_info" VALUES ('C0002', 2, '2018-03-10 12:11:51', 'Go Green Power Smoothie', 'https://earthsown.com/wp-content/uploads/2018/10/Green-Power.jpg', 'Smoothies', 13.00, 0, 108, '2018-03-10 12:11:51');
+--INSERT INTO "public"."product_info" VALUES ('C0002', 2, '2018-03-10 12:11:51', 'Boys Clothes', 'https://d2ul0w83gls0j4.cloudfront.net/taxonomy/300/0102/20171024151632.jpg', 'Shirts', 13.00, 0, 108, '2018-03-10 12:11:51');
+
+
+INSERT INTO "public"."product_info" VALUES ('B0001', 0, '2018-03-10 06:44:25', 'Apples', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/%28A_bunch_of_Royal_Gala_Apples_from_Chile%29.JPG/1200px-%28A_bunch_of_Royal_Gala_Apples_from_Chile%29.JPG', 'Apples are delecious', 30.00, 0, 96, '2018-03-10 06:44:25');
+--INSERT INTO "public"."product_info" VALUES ('B0001', 0, '2018-03-10 06:44:25', 'Books for learning Java', 'https://images-na.ssl-images-amazon.com/images/I/41f6Rd6ZEPL._SX363_BO1,204,203,200_.jpg', 'Core Java', 30.00, 0, 96, '2018-03-10 06:44:25');
+
+INSERT INTO "public"."product_info" VALUES ('B0004', 0, '2018-03-10 10:39:29', 'Mangoes', 'https://upload.wikimedia.org/wikipedia/commons/e/ee/Mango_and_cross_section_edit.jpg', 'Mangoes are tasty', 30.00, 0, 199, '2018-03-10 10:39:32');
+--INSERT INTO "public"."product_info" VALUES ('B0004', 0, '2018-03-10 10:39:29', 'Java SE', 'https://www.pearsonhighered.com/assets/bigcovers/0/1/3/2/0132778041.jpg', 'Effective Java', 30.00, 0, 199, '2018-03-10 10:39:32');
+
+INSERT INTO "public"."product_info" VALUES ('B0005', 0, '2018-03-10 10:40:35', 'Peach', 'https://image.freepik.com/free-photo/peach-fruit-slice-isolated-white-background_88281-133.jpg', 'Peaches are tasty', 30.00, 0, 199, '2018-03-10 10:40:35');
+--INSERT INTO "public"."product_info" VALUES ('B0005', 0, '2018-03-10 10:40:35', 'Java SE', 'https://images-na.ssl-images-amazon.com/images/I/51S8VRHA2FL._SX357_BO1,204,203,200_.jpg', 'Thinking in Java', 30.00, 0, 199, '2018-03-10 10:40:35');
+
+
 INSERT INTO "public"."product_info" VALUES ('D0002', 3, '2018-03-10 12:08:17', 'Awesome', 'https://starbuckssecretmenu.net/wp-content/uploads/2017/06/Starbucks-Violet-Drink.jpg', 'Starbucks Violet Drink', 2.00, 0, 200, '2018-03-10 12:08:17');
-INSERT INTO "public"."product_info" VALUES ('F0001', 1, '2018-03-10 12:15:05', 'Kids Party Food', 'http://asset1.marksandspencer.com/is/image/mands/MS_FD_F04C_00398503_NC_X_EC_0?$PDP_MAXI_ZOOM_NEW$', 'Chicken', 4.00, 0, 57, '2018-03-10 12:15:10');
-INSERT INTO "public"."product_info" VALUES ('F0002', 1, '2018-03-10 12:16:44', 'Family s', 'http://cdn1.thecomeback.com/wp-content/uploads/2017/05/mcdonalds_food-832x447.png', 'McDonald‘s Food', 20.00, 0, 22, '2018-03-10 12:16:44');
+
+INSERT INTO "public"."product_info" VALUES ('F0001', 1, '2018-03-10 12:15:05', 'Tomatoes', 'https://thumbnails.yayimages.com/1600/0/7ef/7efc82.jpg', 'Tomatoes are good', 4.00, 0, 57, '2018-03-10 12:15:10');
+--INSERT INTO "public"."product_info" VALUES ('F0001', 1, '2018-03-10 12:15:05', 'Kids Party Food', 'http://asset1.marksandspencer.com/is/image/mands/MS_FD_F04C_00398503_NC_X_EC_0?$PDP_MAXI_ZOOM_NEW$', 'Chicken', 4.00, 0, 57, '2018-03-10 12:15:10');
+
+INSERT INTO "public"."product_info" VALUES ('F0002', 1, '2018-03-10 12:16:44', 'Cucumbers', 'https://img.freepik.com/free-photo/sliced-cucumbers-white-background_23-2147829036.jpg?size=664&ext=jpg', 'Cucumbers are tasty', 20.00, 0, 22, '2018-03-10 12:16:44');
+--INSERT INTO "public"."product_info" VALUES ('F0002', 1, '2018-03-10 12:16:44', 'Family s', 'http://cdn1.thecomeback.com/wp-content/uploads/2017/05/mcdonalds_food-832x447.png', 'McDonald‘s Food', 20.00, 0, 22, '2018-03-10 12:16:44');
+
+
+INSERT INTO "public"."product_info" VALUES ('F0003', 1, '2018-03-10 12:16:44', 'Onions', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Onions.jpg/1200px-Onions.jpg', 'onions', 120.00, 0, 22, '2018-03-10 12:16:44');
 
 
 -- ----------------------------
